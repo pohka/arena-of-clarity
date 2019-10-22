d727163127f402c096249f7a12eb1eef9e039638
geffLaptop <zomg21@hotmail.com>
2019-10-22 17:56:50 +0100

    inital commit

---
+++ scripts/vscripts/item_armor_tiered_modifier.lua
@@ -0,0 +1,29 @@
+item_armor_tiered_modifier = class({})
+
+function item_armor_tiered_modifier:OnCreated()
+  if IsServer() then
+    print("created amoror modifier")
+  end
+end
+
+function item_armor_tiered_modifier:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_HEALTH_BONUS
+  }
+  return funcs
+end
+
+function item_armor_tiered_modifier:IsHidden()
+  return true
+end
+
+function item_armor_tiered_modifier:GetModifierHealthBonus()
+  local abil = self:GetAbility()
+  if abil ~= nil then
+    local bonusHP = abil:GetSpecialValueFor("hp")
+    if bonusHP ~= nil then
+      return bonusHP
+    end
+  end
+  return 0
+end
